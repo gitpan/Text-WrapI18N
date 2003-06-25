88e130cb16c1d4ef8723c760202aab9a3e16db0c
久保田 智広 <kubota@debian.org>
2003-06-25 14:20:35 +0000

    Import of KUBOTA/Text-WrapI18N-0.06 from CPAN.
    gitpan-cpan-distribution: Text-WrapI18N
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         KUBOTA/Text-WrapI18N-0.06.tar.gz
    gitpan-cpan-author:       KUBOTA
    gitpan-cpan-maturity:     released

--- WrapI18N.pm
+++ WrapI18N.pm
@@ -9,7 +9,7 @@ our @EXPORT = qw(wrap);
 our @EXPORT_OK = qw($columns $separator);
 our %EXPORT_TAGS = ('all' => [ @EXPORT, @EXPORT_OK ]);
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use vars qw($columns $break $tabstop $separator $huge $unexpand $charmap);
 use Text::CharWidth qw(mbswidth mblen);
